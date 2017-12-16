a3cb68246c634bcc8efa6371d05b4fe4ebc44221
kempa <dariusz.kempa@nokia.com>
2017-12-16 21:56:18 +0100

    added board

---
+++ src/elements/board.rs
@@ -0,0 +1,5 @@
+
+
+struct Board{
+    pub fields :  Box<[i32]>
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@ extern crate piston_window;
 #[macro_use] extern crate serde_derive;
 
 use piston_window::*;
-
+use std::fs::File;
+use std::io::prelude::*;
 mod config;
-mod board;
+
+
+extern crate serde_json;
+
+fn load_config() -> config::Config
+{
+    return  File::open("resources/config.json").ok().and_then(|mut f| {
+        let mut s = String::new();
+        f.read_to_string(&mut s).ok().and_then(|_| serde_json::from_str(&s).ok())
+    }).unwrap_or_default();
+}
 
 fn main() {
 
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480))
+
+    let config: config::Config = load_config();
+
+    let mut window: PistonWindow = WindowSettings::new(config.title.clone(), (640, 480))
         .exit_on_esc(true)
         .build()
         .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
